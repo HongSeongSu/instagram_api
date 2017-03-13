9b7ed644095dda30a485476788dc5fbcc9ff074f
sshong1991 <sshong1991@gmail.com>
2017-03-13 16:30:48 +0900

    urls setting

--- django_app/post/views.py
+++ django_app/post/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+
+from post.models import Post
+
+User = get_user_model()
+
 
 # Create your views here.
+
+
+@csrf_exempt
+def post_create(request):
+    if request.method == 'POST':
+        try:
+            author_id = request.POST['author_id']
+            author = User.objects.get(id=author_id)
+        except KeyError:
+            return HttpResponse('key "author_id" is required field')
+        except User.DoesNotExist:
+            return HttpResponse('author_id {} is not exist'.format(
+                request.POST['author_id']
+            ))
+
+        post = Post.objects.create(author=author)
+        return HttpResponse('{}'.format(post.pk))
+    else:
+        return HttpResponse('Post create view')
